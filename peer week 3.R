996b898ba4f4b6e9b384f3415c88e0f39119e2fd
Aditya Kumar Singh <38782886+adi19012001@users.noreply.github.com>
2020-06-29 14:12:41 +0530

    Add files via upload

---
+++ peer week 3.R
@@ -0,0 +1,33 @@
+##Aditya Singh
+# week 3 peer graded asignment
+
+makeCacheMatrix <- function(x = matrix()) {
+  j <- NULL
+  set <- function(y){
+    x <<- y
+    j <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) j <<- inverse
+  getInverse <- function() j 
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
+}
+
+#cachesolve
+
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  j <- x$getInverse()
+  if(!is.null(j)){
+    message("getting cached data")
+    return(j)
+  }
+  mat <- x$get()
+  j <- solve(mat,...)
+  x$setInverse(j)
+  j
+}
+
+#execute
